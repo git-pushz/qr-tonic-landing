d19c36c89822d45949dd970a6370e42de0664ec3
Lorenzo Cravero <lorenzocravero@protonmail.com>
2025-04-10 22:01:59 +0200

    Add configs for github pages

--- .eleventy.js
+++ .eleventy.js
@@ -16,5 +16,6 @@ module.exports = function(eleventyConfig) {
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
     dataTemplateEngine: "njk",
+    pathPrefix: process.env.NODE_ENV === 'production' ? '/qr-tonic-landing/' : '/'
   };
 }; 
