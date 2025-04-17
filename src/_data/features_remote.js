ed52fcaadff7d81fee90cbf50da2bda967d51725
Lorenzo Cravero <lorenzocravero@protonmail.com>
2025-04-17 21:27:26 +0200

    Add JSDoc

--- src/_data/features_remote.js
+++ src/_data/features_remote.js
@@ -1,6 +1,102 @@
+/**
+ * @typedef {Object} BusinessTrait
+ * @property {string} title
+ * @property {string} text
+ * @property {string} image
+ */
+
+/**
+ * @typedef {Object} Review
+ * @property {string} avatar
+ * @property {string} name
+ * @property {string} text
+ */
+
+/**
+ * @typedef {Object} Experience
+ * @property {string} title
+ * @property {string} text
+ * @property {string} image
+ */
+
+/**
+ * @typedef {Object} Location
+ * @property {number} latitude
+ * @property {number} longitude
+ */
+
+/**
+ * @typedef {Object} Footer
+ * @property {string} email
+ * @property {string} phone
+ * @property {Location} location
+ */
+
+/**
+ * @typedef {Object} Hero
+ * @property {boolean} enabled
+ * @property {string} title
+ * @property {string} subtitle
+ * @property {Object} background
+ * @property {string} background.fallback
+ * @property {string} background.video
+ */
+
+/**
+ * @typedef {Object} WhoWeAre
+ * @property {boolean} enabled
+ * @property {string} title
+ * @property {string} subtitle
+ * @property {BusinessTrait[]} businessTraits
+ */
+
+/**
+ * @typedef {Object} Testimonials
+ * @property {boolean} enabled
+ * @property {string} title
+ * @property {string} subtitle
+ * @property {Review[]} reviews
+ */
+
+/**
+ * @typedef {Object} Experiences
+ * @property {boolean} enabled
+ * @property {Experience[]} items
+ */
+
+/**
+ * @typedef {Object} Sections
+ * @property {Hero} hero
+ * @property {WhoWeAre} whoWeAre
+ * @property {Testimonials} testimonials
+ * @property {Experiences} experiences
+ */
+
+/**
+ * @typedef {Object} Logo
+ * @property {string} src
+ * @property {string} alt
+ */
+
+/**
+ * @typedef {Object} FeaturesDoc
+ * @property {Sections} sections
+ * @property {Footer} footer
+ */
+
+/**
+ * @typedef {Object} Features
+ * @property {FeaturesDoc} _doc
+ * @property {Logo} logo
+ */
+
 const Fetch = require('@11ty/eleventy-fetch');
 require('dotenv').config();
 
+/**
+ * @returns {Features}
+ * @throws {Error}
+ */
 module.exports = async function() {
   const CONFIGURATION_ENDPOINT = process.env.CONFIGURATION_ENDPOINT;
 
@@ -9,6 +105,7 @@ module.exports = async function() {
   }
 
   try {
+    /** @type {Features} */
     const data = await Fetch(CONFIGURATION_ENDPOINT, {
       duration: "1d",
       type: "json"
